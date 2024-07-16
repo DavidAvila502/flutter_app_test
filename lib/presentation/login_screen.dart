9eb6ff16aba7ddf72d40adf2eca3a1298747ae63
David Avila <davidavila6002@gmail.com>
2024-07-16 13:25:33 -0600

    login screen ready

--- lib/presentation/login_screen.dart
+++ lib/presentation/login_screen.dart
@@ -16,31 +16,49 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Consumer<AuthProvider>(
         builder: (context, value, child) => Scaffold(
-              appBar: AppBar(
-                backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-                title: Text(widget.title),
-              ),
-              body: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text('Welcome!',
-                        style: TextStyle(
-                            fontSize: 22, fontWeight: FontWeight.bold)),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        value.login();
-                      },
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.white,
-                        backgroundColor: Colors.blue[500],
+              // appBar: AppBar(
+              //   backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+              //   title: Text(widget.title),
+              // ),
+              body: Container(
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/poke_background.jpg'),
+                        fit: BoxFit.cover)),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('Welcome!',
+                          style: TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white)),
+                      const SizedBox(
+                        height: 20,
                       ),
-                      child: const Text('Login'),
-                    ),
-                  ],
+                      GestureDetector(
+                        onTap: () => value.login(),
+                        child: Container(
+                          decoration: const BoxDecoration(
+                              color: Colors.black,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10))),
+                          child: const Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 40, vertical: 5),
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ));
